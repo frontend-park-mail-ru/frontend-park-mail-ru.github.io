8fad4505db498bf3aa193150a6200e82ca0e5476
Anton Eprev <a.eprev@gmail.com>
2014-02-24 01:01:29 +0400

    Update

---
+++ d/ajax/index.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
+
+app.get('/ping', function(req, res) {
+    var body = 'pong';
+    if (req.headers.origin) {
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    }
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    res.end(body);
+});
+
+app.post('/echo', function(req, res) {
+    var body = req.body.msg || '';
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    res.end(body);
+});
+
+app.post('/ifecho', function(req, res) {
+    var body = '<script>window.parent.' + req.body.cb + '(' + JSON.stringify(req.body.msg) + ');</script>';
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    res.end(body);
+});
+
+app.get('/10s', function(req, res) {
+    setTimeout(function () {
+        var body = 'It took 10 seconds to respond!';
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Length', Buffer.byteLength(body));
+        res.end(body);
+    }, 10000);
+});
+
+app.get('/jsonp', function(req, res) {
+    var body = req.query.cb + '({message: "Hello World"});';
+    res.setHeader('Content-Type', 'text/javascript');
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    res.end(body);
+});
+
+app.post('/upload', function(req, res) {
+    console.log(req);
+    var body = 'pong';
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    res.end(body);
+});
+
+app.listen(3000);
