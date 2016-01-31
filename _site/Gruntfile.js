a9c0fd83ba802452cb1740fb900154d2fe5cf84c
Artem Mezin <iketari@gmail.com>
2016-01-31 23:05:38 +0300

    lecture 1. theoretical part

--- _site/Gruntfile.js
+++ _site/Gruntfile.js
@@ -21,48 +21,10 @@ module.exports = function (grunt) {
 					drafts: true
 				}
 			}
-		},
-
-		markdown: {
-			all: {
-				files: [
-					{
-						expand: true,
-						src: '**/*.md',
-						dest: '_site/',
-						ext: '.html'
-					},
-					{
-						src: 'index.md',
-						dest: './index.html',
-						ext: '.html'
-					}
-				],
-				options: {
-					template: '_layouts/default.jst',
-					preCompile: function(src, context) {},
-					postCompile: function(src, context) {},
-					templateContext: {
-						site_title: 'Технопарк@Mail.Ru. Фронтенд разработка.',
-						page_title: ''
-					},
-					contextBinder: false,
-					contextBinderMark: '@@@',
-					markdownOptions: {
-						gfm: true,
-						highlight: 'manual',
-						codeLines: {
-							before: '<span>',
-							after: '</span>'
-						}
-					}
-				}
-			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-jekyll');
-	grunt.loadNpmTasks('grunt-markdown');
 
 	grunt.registerTask('default', ['jekyll:serve']);
 
